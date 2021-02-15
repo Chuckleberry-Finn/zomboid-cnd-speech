ec53b476a8072252adfc1a545e3385d736149b5f
ChuckTheJittai <ChuckTheSheep@users.noreply.github.com>
2021-02-14 21:46:29 -0500

    Clean up file structure #4
    Fixes: Clean up file structure #4

---
+++ media/lua/client/ConditionalSpeech__Util.lua
@@ -0,0 +1,69 @@
+
+
+--------------------------- USEFUL FUNCTIONS ------------------------------
+--useful function to check if value in list
+function valueIn(tab, val)
+	if not tab or not val then return false end
+	for _,value in pairs(tab) do if value == val then return true end end
+	return false
+end
+
+--useful function to check if key in list
+function keyIn(tab, k)
+	if not tab or not k then return false end
+	for key,_ in pairs(tab) do if key == k then return true end end
+	return false
+end
+
+--useful function to pick a random entry
+function pickFrom(list) return list[(ZombRand(#list)+1)] end
+
+-- Useful replace text function
+function replaceText(text, findthis, replacewith)
+	if not text or not findthis or not replacewith then return end
+	return text:gsub(findthis, replacewith)
+end
+
+-- Useful probability function - note: 101 = 0 to 100
+function prob(x) if ZombRand(101) < x then return true else return false end end
+
+-- Useful text splitter function - returns a list of words
+function splitTextbyWord(text, separator)
+	if not text then return end
+	if not separator then separator = "%s" end
+	local t={}
+	for word in string.gmatch(text, "([^"..separator.."]+)") do table.insert(t, word) end
+	return t
+end
+
+-- Useful text splitter function - returns a list of characters
+function splitTextbyChar(text)
+	if not text then return end
+	local t={}
+	for i = 1, #text do table.insert(t, text:sub(i,i)) end
+	return t
+end
+
+function joinText(list, spaced)
+	if not list then return end
+	local t = ""
+	for key,value in pairs(list) do
+		t = t .. value
+		if spaced > 0 and key ~= #list then t = t .. " " end
+	end
+	return t
+end
+
+-- Weighted Random Pick from list
+function WeightedRandPick(table,intensity,maxintensity)
+	if not table then return end
+	local pick = (ZombRand(math.floor(#table/maxintensity))*intensity)+1 -- +1 to offset 0 start
+	if pick <= 0 then pick = 1 end
+	if pick > #table then pick = #table end
+	pick = table[pick]
+	if pick then
+		return pick
+	else
+		return nil
+	end
+end
