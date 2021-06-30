430691c18983da021ac5d13d2055aa470c20fa71
ChuckTheJittai <ChuckTheSheep@users.noreply.github.com>
2021-06-30 00:42:15 -0400

    Implement config

---
+++ Contents/mods/zomboid-cnd-speech/media/lua/client/ConditionalSpeech__Config.lua
@@ -0,0 +1,21 @@
+require "_EasyConfig_Chucked"
+require "OptionScreens/ServerSettingsScreen"
+require "OptionScreens/SandBoxOptions"
+
+cndSpeechConfig = cndSpeechConfig or {}
+
+cndSpeechConfig.config = {
+	NPCsDontTalk = false,
+}
+
+cndSpeechConfig.modId = "Conditional-Speech" -- needs to the same as in your mod.info
+cndSpeechConfig.name = "Conditional Speech" -- the name that will be shown in the MOD tab
+
+cndSpeechConfig.menu = {
+	NPCsDontTalk = {type = "Tickbox", title = "Disable Non-Players' Speech", tooltip = "Non-player characters (NPCs) don't speak with Conditional Speech.",},
+}
+
+--load mod into EasyConfig
+if EasyConfig_Chucked then
+	EasyConfig_Chucked.addMod(cndSpeechConfig.modId, cndSpeechConfig.name, cndSpeechConfig.config, cndSpeechConfig.menu, "CONDITIONAL SPEECH")
+end
