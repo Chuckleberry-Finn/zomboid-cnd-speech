397ee561145b4836958b324b9966a225b6df12e3
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2023-11-14 19:31:17 -0500

    Update ConditionalSpeech06_client.lua

--- Contents/mods/zomboid-cnd-speech/media/lua/client/ConditionalSpeech06_client.lua
+++ Contents/mods/zomboid-cnd-speech/media/lua/client/ConditionalSpeech06_client.lua
@@ -7,6 +7,7 @@ local function onServerCommand(_module, _command, _data)
 end
 Events.OnServerCommand.Add(onServerCommand)--/server/ to client
 
-local modCountSystem = require "chuckleberryFinnModding_modCountSystem"
-if modCountSystem then modCountSystem.pullAndAddModID()
-else print("ERR: MISSING MOD: `ChuckleberryFinnAlertSystem` (Workshop ID: `3077900375`)") end
+if getActivatedMods():contains("ChuckleberryFinnAlertSystem") then
+    local modCountSystem = require "chuckleberryFinnModding_modCountSystem"
+    if modCountSystem then modCountSystem.pullAndAddModID() end
+else print("WARNING: Highly recommended to install `ChuckleberryFinnAlertSystem` (Workshop ID: `3077900375`) for latest news and updates.") end
